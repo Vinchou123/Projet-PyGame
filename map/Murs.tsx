d4e8405320f241f9fec5ccdc7c002515b0799a05
Vincenzo <vincent.luu@ynov.com>
2025-01-16 15:35:50 +0100

    add ghosts moove random

---
+++ map/Murs.tsx
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Murs" tilewidth="32" tileheight="32" tilecount="41" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_border_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_border_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_border_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_border_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch1_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch1_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch1_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch1_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch2_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch2_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch2_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch2_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch3_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch3_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch3_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch3_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch4_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch4_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch4_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_branch4_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner1_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner1_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner1_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner1_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="24">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner2_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner2_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner2_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="27">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner2_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="28">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner3_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="29">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner3_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="30">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner3_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="31">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_corner3_4.png" width="32" height="32"/>
+ </tile>
+ <tile id="32">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_full.png" width="32" height="32"/>
+ </tile>
+ <tile id="33">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_horizontal_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="34">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_horizontal_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="35">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_horizontal_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="36">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_intersection.png" width="32" height="32"/>
+ </tile>
+ <tile id="37">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_point.png" width="32" height="32"/>
+ </tile>
+ <tile id="38">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_vertical_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="39">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_vertical_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="40">
+  <image source="pacman/texturepack/basic_texturepack_x32/tiles/wall/wall_vertical_3.png" width="32" height="32"/>
+ </tile>
+</tileset>
